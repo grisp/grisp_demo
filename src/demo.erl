617b64e7019e42cc094cdfad9b416600fa14169f
Mirjam Friesen <mirjam.friesen@stritzinger.com>
2021-11-26 12:06:35 +0100

    Add blinking light

--- src/demo.erl
+++ src/demo.erl
@@ -10,7 +10,17 @@
 %--- Callbacks -----------------------------------------------------------------
 
 % @private
-start(_Type, _Args) -> demo_sup:start_link().
+start(_Type, _Args) ->
+    {ok, Supervisor} = demo_sup:start_link(),
+    LEDs = [1, 2],
+    [grisp_led:flash(L, red, 500) || L <- LEDs],
+    timer:sleep(5000),
+    grisp_led:off(2),
+    Random = fun() ->
+        {rand:uniform(2) - 1, rand:uniform(2) -1, rand:uniform(2) - 1}
+    end,
+    grisp_led:pattern(1, [{500, Random}]),
+    {ok, Supervisor}.
 
 % @private
 stop(_State) -> ok.
